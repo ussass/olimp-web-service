24f714e5bce229d967f246e70f90866bd9ff4b51
Trofimov <mikletrof@gmail.com>
2022-02-15 15:34:21 +0300

    added exception filter

--- src/main/kotlin/bet/olimp/interview/ApiStart.kt
+++ src/main/kotlin/bet/olimp/interview/ApiStart.kt
@@ -1,6 +1,9 @@
 package bet.olimp.interview
 
+import bet.olimp.interview.entity.ErrorDescription
 import bet.olimp.interview.handler.*
+import org.http4k.core.*
+import org.http4k.format.Jackson.auto
 import org.http4k.routing.routes
 import org.http4k.server.Undertow
 import org.http4k.server.asServer
@@ -9,7 +12,20 @@ object ApiStart {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        routes(
+
+        val catchAll = Filter { nextHandler ->
+            val wrapperHandler: HttpHandler = { request ->
+                try {
+                    nextHandler(request)
+                } catch (e: Exception) {
+                    val errorDescription = ErrorDescription(e.javaClass.name, e.message, e.stackTrace)
+                    Body.auto<ErrorDescription>().toLens().inject(errorDescription, Response(Status.INTERNAL_SERVER_ERROR))
+                }
+            }
+            wrapperHandler
+        }
+
+        val httpHandler: HttpHandler = routes(
             pingGetHandler,
             pingPostHandler,
             getAll,
@@ -17,7 +33,7 @@ object ApiStart {
             saveUser,
             updateUser,
             deleteUser
-        ).asServer(Undertow(8080))
-            .start()
+        ).withFilter(catchAll)
+        httpHandler.asServer(Undertow(8080)).start()
     }
 }
